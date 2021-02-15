8a8b0ce98f97c52f72d58d7f51f1bd29a80b1424
Anna Cavalla <anna.cavalla@gmail.com>
2021-02-15 16:45:42 +0000

    css'd the form a bit, console log printing

---
+++ interface.js
@@ -0,0 +1,12 @@
+var elements = document.querySelector('#list-all');
+
+
+let list = new List();
+list.createNote("testings");
+console.log(list)
+
+list.showNotes().forEach(function(note){
+  note;
+  // console.log(i);
+
+}
