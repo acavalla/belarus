3d0dbc382b7a6426de6c30590fc69b00e8158581
Anna Cavalla <anna.cavalla@gmail.com>
2021-02-16 15:08:47 +0000

    shows full notes in hash url

---
+++ src/hashLogic.js
@@ -0,0 +1,20 @@
+makeUrlChangeShowNoteForCurrentPage();
+
+function makeUrlChangeShowNoteForCurrentPage() {
+  window.addEventListener("hashchange", showNoteForCurrentPage);
+};
+
+function showNoteForCurrentPage() {
+  showNote(getNoteFromUrl(window.location));
+};
+
+function getNoteFromUrl(location) {
+  a = location.hash.split("#")[1];
+  return list.showNotes()[a - 1];
+};
+
+function showNote(note) {
+  document
+    .getElementById("printed-note")
+    .innerHTML = note;
+};
